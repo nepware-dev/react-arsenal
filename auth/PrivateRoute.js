95f64f129c7ce6a563e620525f8bfa6083dba78d
Bishal Timilsina <bishaltimilsina@gmail.com>
2019-07-04 15:30:41 +0545

    Initial commit

---
+++ auth/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+
+export const PrivateRoute = ({
+  component: Component,
+  isAuthenticated, ...rest }
+) => {
+  console.log(isAuthenticated);
+  return (
+    <Route {...rest} render={props => (
+      isAuthenticated || (localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")).isAuthenticated : false) ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{
+          pathname: '/login',
+          state: { from: props.location }
+        }} />
+      )
+    )} />
+  )
+}

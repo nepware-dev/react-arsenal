2c3affffbc14fdfeae79eda61f6c207bbe1bdcec
Dipesh Lamichhane <ft9dipesh@gmail.com>
2021-01-29 14:58:09 +0545

    Accordion component

---
+++ components/Accordion/index.js
@@ -0,0 +1,75 @@
+import React, {useRef, useCallback, useState} from 'react';
+import PropTypes from 'prop-types';
+
+import {FiChevronRight} from 'react-icons/fi';
+
+import cs from '../../cs';
+import styles from './styles.module.scss';
+
+const propTypes = {
+    /*
+     * Title of the accordion. Is clickable and opens the content.
+     * Not used when renderHeader is passed.
+     */
+    title: PropTypes.string,
+    /*
+     * Custom renderer for the accordion header.
+     * Called with isExpanded denoting whether accordion content is currently visible or not.
+     */
+    renderHeader: PropTypes.func,
+    /*
+     * Class applied to the container for the accordion.
+     */
+    className: PropTypes.string,
+    /*
+     * Class applied to the title of the accordion when not custom rendered.
+     */
+    titleClassName: PropTypes.string,
+    children: PropTypes.any,
+};
+
+const Accordion = ({
+    title, 
+    children, 
+    className, 
+    renderHeader,
+    titleClassName,
+}) => {
+    const [active, setActive] = useState(false);
+    const [height, setHeight] = useState('0px');
+
+    const content = useRef();
+
+    const toggleAccordion = useCallback(() => {
+        setHeight(active ? '0px' : `${content.current.scrollHeight}px`);
+        setActive(!active);
+    }, [active]);
+
+    return (
+        <div className={cs(styles.accordionSection, className)}>
+            <div className={styles.accordion} onClick={toggleAccordion}>
+                {renderHeader ? renderHeader({isExpanded: active}) : (
+                    <div className={cs(styles.accordionTitle, titleClassName)}>
+                        {title}
+                        <FiChevronRight 
+                            className={cs(styles.rightIcon, {[styles.rotateUp]: active})} 
+                        />
+                    </div>
+                )}
+            </div>
+            <div
+                ref={content}
+                style={{maxHeight: height}}
+                className={cs(styles.accordionContent, {
+                    [styles.accordionContentActive]: active
+                })}
+            >
+                {children}
+            </div>
+        </div>
+    );
+};
+
+Accordion.propTypes = propTypes;
+
+export default Accordion;

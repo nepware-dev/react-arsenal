10651916749322a8071fc2f78ccef8f9b7f8206a
ZenithMaharjan <147016905+ZenithMaharjan@users.noreply.github.com>
2026-02-09 11:22:24 +0545

    refactor: convert List to functional component
    - [x] add support for React.Fragment via component prop
    - [x] remove forwardRef support

--- components/List/types.ts
+++ components/List/types.ts
@@ -1,13 +1,12 @@
-import * as React from 'react';
+import React, { type JSX } from "react";
 
-import type { JSX } from "react";
-
-export type ListRenderItemProps<T> = {item: T; index: number;};
+export type ListRenderItemProps<T> = { item: T; index: number };
 
 export type KeyExtractor<T> = (item: T, index: number) => string | number;
 export type ListRenderItem<T> = (props: ListRenderItemProps<T>) => React.ReactNode;
 
 export interface ListProps<T> {
+    ref?: React.Ref<HTMLDivElement>;
     className?: string;
     style?: React.CSSProperties;
     classNameItem?: string;
@@ -20,13 +19,9 @@ export interface ListProps<T> {
     onEndReached?: () => void;
     onClick?: React.MouseEventHandler;
     onItemClick?: React.MouseEventHandler;
-    component?: keyof JSX.IntrinsicElements;
-    EmptyComponent?: React.ReactNode;
-    LoadingComponent?: React.ReactNode;
-    HeaderComponent?: React.ReactNode;
-    FooterComponent?: React.ReactNode;
+    component?: keyof JSX.IntrinsicElements | typeof React.Fragment;
+    EmptyComponent?: React.ReactNode | React.ElementType;
+    LoadingComponent?: React.ReactNode | React.ElementType;
+    HeaderComponent?: React.ReactNode | React.ElementType;
+    FooterComponent?: React.ReactNode | React.ElementType;
 }
-
-declare const List;
-
-export default List;
